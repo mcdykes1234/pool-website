6124b61567ab90a263a902e2cbfcf93e1a7f4c59
Mary <mcdykes1234@yahoo.com>
2026-07-07 17:26:33 -0500

    Initial website upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// List of background images
+const images = [
+  "images/pool1.jpg",
+  "images/pool2.jpg",
+  "images/pool3.jpg"
+];
+
+let currentIndex = 0;
+
+// Set initial background
+function updateImage() {
+document.querySelector(".image-slider").style.backgroundImage =
+  `url(${images[currentIndex]})`;
+}
+
+updateImage();
+
+function nextImage() {
+  currentIndex = (currentIndex + 1) % images.length;
+  document.querySelector(".image-slider").style.backgroundImage =
+    `url(${images[currentIndex]})`;
+}
+
+function prevImage() {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  document.querySelector(".image-slider").style.backgroundImage =
+    `url(${images[currentIndex]})`;
+}
