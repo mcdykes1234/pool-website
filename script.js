04d483dcf52134b64b353c9a2bc02d8138dbcb70
Mary <mcdykes1234@yahoo.com>
2026-07-09 11:50:06 -0500

    Update

--- script.js
+++ script.js
@@ -2,27 +2,63 @@
 const images = [
   "images/pool6.jpg",
   "images/poolOne.jpg",
-  "images/poolTwo.jpg"
+  "images/poolLesson2.jpg"
 ];
 
 let currentIndex = 0;
 
-// Set initial background
 function updateImage() {
-document.querySelector(".image-slider").style.backgroundImage =
-  `url(${images[currentIndex]})`;
+  const slider = document.querySelector(".image-slider");
+
+  // Set the background image
+  slider.style.backgroundImage = `url(${images[currentIndex]})`;
+
+  // Remove the special class every time
+  slider.classList.remove("bottom-half");
+
+  // If the current image is the one you want to crop, add the class
+  if (images[currentIndex].includes("poolLesson2.jpg")) {
+    slider.classList.add("bottom-half");
+  }
 }
 
-updateImage();
+if (document.querySelector(".image-slider")) {
+  updateImage();
+}
 
 function nextImage() {
   currentIndex = (currentIndex + 1) % images.length;
-  document.querySelector(".image-slider").style.backgroundImage =
-    `url(${images[currentIndex]})`;
+  updateImage();
 }
 
 function prevImage() {
   currentIndex = (currentIndex - 1 + images.length) % images.length;
-  document.querySelector(".image-slider").style.backgroundImage =
-    `url(${images[currentIndex]})`;
+  updateImage();
+}
+
+
+const lessonImages = [
+  "images/poolLesson1.jpg",
+  "images/poolLesson3.jpg",
+  "images/poolLesson4.jpg",
+  "images/poolTwo.jpg"
+];
+
+let lessonIndex = 0;
+
+function updateLessonImage() {
+  document.querySelector(".lessons-slider").style.backgroundImage =
+    `url(${lessonImages[lessonIndex]})`;
+}
+
+updateLessonImage();
+
+function nextLessonImage() {
+  lessonIndex = (lessonIndex + 1) % lessonImages.length;
+  updateLessonImage();
+}
+
+function prevLessonImage() {
+  lessonIndex = (lessonIndex - 1 + lessonImages.length) % lessonImages.length;
+  updateLessonImage();
 }
